f9bb546ed6650ba5256e09370f96c3bd8ab4a61e
kobii <harold.rockata@gmail.com>
2023-07-15 23:26:36 +0200

    Downloads react frontend with tag from github account

--- build-react-ui.sh
+++ build-react-ui.sh
@@ -1,19 +1,8 @@
 #! /usr/bin/env bash
 
-# npm install
-# # git clone git@github.com:kobesani/react-phonebook.git
-# # echo "cloned react-phonebook repo"
-curl -L https://github.com/kobesani/react-phonebook/archive/refs/heads/main.zip -o main.zip
+rm -rf ./build
+curl -L https://github.com/kobesani/react-phonebook/releases/download/v0.1.5/react-github-actions-release-build.zip -o main.zip
 unzip ./main.zip
 rm -rf ./main.zip
-cd ./react-phonebook-main
-echo "Current working dir ${PWD}"
-# # install react dependencies
-# npm install .
-# # # execute production build and copy to build-test in backend
-# npm run build .
-# cp -r ./build ../build
-cd ../
-ls -lha
-echo "removing react-phonebook-main folder"
-rm -rf "./react-phonebook-main"
+mv ./react-github-actions-build ./build
+
