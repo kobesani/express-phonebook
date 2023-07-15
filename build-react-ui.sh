eb5fc80b72678f86c3131acfcf958a6fffa970a6
kobii <harold.rockata@gmail.com>
2023-07-15 21:42:40 +0200

    Trying wget instead of git clone

--- build-react-ui.sh
+++ build-react-ui.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
 
-git clone git@github.com:kobesani/react-phonebook.git
-echo "cloned react-phonebook repo"
+# git clone git@github.com:kobesani/react-phonebook.git
+# echo "cloned react-phonebook repo"
+wget "https://github.com/kobesani/react-phonebook/archive/refs/heads/main.zip"
+unzip ./main.zip
+# cd ./react-phonebook 
+# # install react dependencies
+# npm install
+# # execute production build and copy to build-test in backend
+# npm run build
+# cp -r ./build ../build-test
