09011bde2496a316404e3d2bbb7bebb11f4188b1
kobii <harold.rockata@gmail.com>
2023-07-15 22:18:23 +0200

    Trying one more time to remove react dir

--- build-react-ui.sh
+++ build-react-ui.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
 
-# git clone git@github.com:kobesani/react-phonebook.git
-# echo "cloned react-phonebook repo"
+# npm install
+# # git clone git@github.com:kobesani/react-phonebook.git
+# # echo "cloned react-phonebook repo"
 curl -L https://github.com/kobesani/react-phonebook/archive/refs/heads/main.zip -o main.zip
 unzip ./main.zip
+rm -rf ./main.zip
 cd ./react-phonebook-main
 echo "Current working dir ${PWD}"
 # # install react dependencies
-npm install
-# # execute production build and copy to build-test in backend
-npm run build
-cp -r ./build ../build
-
-echo "removing react-phonebook-main folder"
-rm -rf react-phonebook-main
-
+# npm install .
+# # # execute production build and copy to build-test in backend
+# npm run build .
+# cp -r ./build ../build
 cd ../
-npm install
+ls -lha
+echo "removing react-phonebook-main folder"
+rm -rf "./react-phonebook-main"
