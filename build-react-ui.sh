2b245659d7e514243960fb7a97d226419763bd69
kobii <harold.rockata@gmail.com>
2023-07-15 23:28:14 +0200

    Trying to use env var FRONTEND_TAG to download specific tagged release

--- build-react-ui.sh
+++ build-react-ui.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
+echo "FRONTEND_TAG=${FRONTEND_TAG}"
 rm -rf ./build
-curl -L https://github.com/kobesani/react-phonebook/releases/download/v0.1.5/react-github-actions-release-build.zip -o main.zip
+curl -L https://github.com/kobesani/react-phonebook/releases/download/${FRONTEND_TAG}/react-github-actions-release-build.zip -o main.zip
 unzip ./main.zip
 rm -rf ./main.zip
 mv ./react-github-actions-build ./build
