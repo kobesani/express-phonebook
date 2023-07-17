599197ccbd65c3338d39275ec879e638199c9fb9
kobii <harold.rockata@gmail.com>
2023-07-17 15:30:56 +0200

    One more quick update to react retrieve script to test change

--- retrieve-react-frontend.sh
+++ retrieve-react-frontend.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-echo "FRONTEND_TAG=${FRONTEND_TAG}"
+echo "FRONTEND_TAG=${FRONTEND_TAG} set via build command"
 rm -rf ./build
 curl -L https://github.com/kobesani/react-phonebook/releases/download/${FRONTEND_TAG}/react-github-actions-release-build.zip -o main.zip
 unzip ./main.zip
